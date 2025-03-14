e4f19bd7b88cc5f8b4fb2895cefe32f150bafe43
jerxmy <jeremy.chambon@mail-esd.com>
2025-03-14 12:11:01 +0100

    components + api

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -14,7 +14,7 @@ function MapComponent({ center }) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={center}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
