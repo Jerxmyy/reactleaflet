a1eca14faa89be648b2f76ce07d7ad5afb4f52d1
jerxmy <jeremy.chambon@mail-esd.com>
2025-03-14 11:31:41 +0100

    euh

---
+++ src/components/MapComponent.jsx
@@ -0,0 +1,26 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function MapComponent({ center }) {
+  return (
+    <>
+      <MapContainer
+        style={{ height: "100vh", width: "100%" }}
+        center={center}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
+}
+export default MapComponent;
